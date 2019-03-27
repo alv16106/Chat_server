f63ce4d83080094754237f8ebe698e43c1ca7fb1
Rodrigo Alvarado <alv16106@uvg.edu.gt>
2019-03-27 12:02:21 -0600

    Server connection and test client

--- server.c
+++ server.c
@@ -5,21 +5,85 @@
 #include <string.h>
 #include <pthread.h>
 #include <sys/types.h>
+#include <arpa/inet.h>
+#include <errno.h>
 #include <netinet/in.h>
 #include "cJSON.h"
+#include "server.h"
 
 #define MAX_CONNECTIONS 10
 #define MESSAGE_BUFFER 1024
 
-typedef struct {
-  int id;
-  char username[25];
-  int status;
-  struct sockaddr_in addr;
-} client;
-
 client connections[MAX_CONNECTIONS];
+int clientNum, id = 0;
 
 int main(int argc, char**argv) {
+  int listenfd = 0, connfd = 0;
+	struct sockaddr_in serv_addr;
+	struct sockaddr_in cli_addr;
+	pthread_t tid;
 
+	/* Socket settings */
+	listenfd = socket(AF_INET, SOCK_STREAM, 0);
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_addr.sin_port = htons(5000);
+
+	/* Bind */
+	if(bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0){
+		handleError("Socket binding failed");
+	}
+
+	/* Listen */
+	if(listen(listenfd, MAX_CONNECTIONS) < 0){
+		handleError("Socket listening failed");
+	}
+
+  printf("<[SERVER STARTED]>\n");
+
+  /* Empezamos a escuchar conexiones de clientes */
+  while(1){
+    socklen_t clilen = sizeof(cli_addr);
+    connfd = accept(listenfd, (struct sockaddr*)&cli_addr, &clilen);
+    if (clientNum > MAX_CONNECTIONS) {
+      printf("Ya se llego al numero maximo de conexiones\n");
+      close(connfd);
+      continue;
+    }
+
+    printf("Incoming connection request:\n");
+		printf("IP Adress: %d\n", cli_addr.sin_addr.s_addr);
+		printf("Port: %d\n", cli_addr.sin_port);
+    printf("ID: %d\n", connfd);
+    client *cli = (client *)malloc(sizeof(client));
+		cli->addr = cli_addr;
+		cli->fd = connfd;
+		cli->id = id++;
+		sprintf(cli->username, "%d", cli->id);
+
+		/* Fork thread */
+		pthread_create(&tid, NULL, &handleSession, (void*)cli);
+
+		/* Reduce CPU usage */
+    sleep(1);
+  }
 }
+
+void handleError(char *ErrorMessage){
+  perror(ErrorMessage);
+  exit(EXIT_FAILURE);
+}
+
+void *handleSession(void *data){
+  char buff[MESSAGE_BUFFER];
+	int readBuff;
+
+	id++;
+	client *cli = (client *)data;
+  printf("Esperando Handshake...\n");
+  readBuff = read(cli->fd, buff, MESSAGE_BUFFER);
+  printf("Valindado... %s\n", buff);
+  strcpy(cli->username, buff);
+  printf("El username ahora es: %s", cli->username);
+}
+
--- server.h
+++ server.h
@@ -1,5 +1,20 @@
-static void handleError(char* message);
-static void *ping(void *data);
-static void *handleSession(void *data);
-static char *sendMessage(char *message, int from, int to);
-static char *sendDiffusion(char *message, int id);
+#include <netinet/in.h>
+
+
+typedef struct {
+  int id;
+  char username[25];
+  int status;
+  int fd;
+  struct sockaddr_in addr;
+} client;
+
+void handleError(char* message);
+void *ping(void *data);
+void *handleSession(void *data);
+char *sendMessage(char *message, int from, int to);
+char *sendDiffusion(char *message, int id);
+char *okHandshake(struct sockaddr_in conn);
+void handleError(char *ErrorMessage);
+void addToQueue(client *cl);
+void removeFromQueue(int id);
